776302166a43d7338a5829617e56070c919c0d7b
Roberto D. Garcia <robertdavid_mgt@hotmail.es>
2019-05-08 10:28:57 -0600

    Primer Push
    Sección 6 - Clase 23

---
+++ wp-content/themes/fiction_university/functions.php
@@ -0,0 +1,21 @@
+<?php 
+	function university_files(){
+		//Cargamos los css o js
+		wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), null, microtime(), true);
+		wp_enqueue_style('font-awosome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+		wp_enqueue_style('google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+		wp_enqueue_style('university_main_styles', get_stylesheet_uri(), NULL, microtime());
+	}
+
+	#Carga el titulo de la pagina actual en las pestañas del navegador
+	function university_features(){
+		register_nav_menu('headerMenuLocation', 'Header Menu Location');
+		register_nav_menu('footerLocationOne', 'Footer Location One');
+		register_nav_menu('footerLocationTwo', 'Footer Location Two');
+		add_theme_support('title-tag');
+	}
+
+	add_action('wp_enqueue_scripts', 'university_files'); #Cargamos los CSS y JS
+	add_action('after_setup_theme', 'university_features'); #Cargamos el hooks para le titulo de las pestañas
+	
+?> 
